25e925e4cc0f5564279b7305fcc2c02c89d430e9
Thomas <tom@beastpc.localdomain>
2020-09-08 19:19:09 -0500

    contact me page

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -6,15 +6,36 @@
 
     <p>I do not have a form for contact. Probably because I don't care for it.</p>
 
-    <p>If you need to get in contact with me, email me: tom@thomasj.me</p>
+    <p>
+      If you need to get in contact with me, email me:
+      <span v-if="!clicked" @mousedown="showEmail">
+        {{ email }}
+      </span>
+      <pre v-else>{{ email }}</pre>
+    </p>
   </div>
 </template>
 
 <script>
   import Navbar from '../components/Navbar'
 export default {
+    data() {
+      return {
+        clicked: false,
+        email: "[click to view]"
+      }
+    },
     components: {
       'navbar': Navbar
+    },
+    methods: {
+        showEmail() {
+          if(!this.clicked) {
+            console.log('clicked')
+            this.email = "tom@thomasj.me"
+            this.clicked = true
+          }
+        }
     }
 }
 </script>
